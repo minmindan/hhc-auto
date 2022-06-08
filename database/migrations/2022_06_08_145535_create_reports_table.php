55fe5dddace545249b61dcde9ed336d021e0cfc7
Xavier <94537420+minmindan@users.noreply.github.com>
2022-06-08 23:13:47 +0800

    krlove
    建立Models

--- database/migrations/2022_06_08_145535_create_reports_table.php
+++ database/migrations/2022_06_08_145535_create_reports_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('report', function (Blueprint $table) {
+        Schema::create('reports', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('report');
+        Schema::dropIfExists('reports');
     }
 };
