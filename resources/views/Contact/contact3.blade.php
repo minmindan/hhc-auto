c162bc6f8b3dd1b56fd0b286fe0709c6e34bcfdd
hetRich <95585077+heyIamYi@users.noreply.github.com>
2022-06-18 12:06:50 +0800

    0618 1206
    update contact image

--- resources/views/Contact/contact3.blade.php
+++ resources/views/Contact/contact3.blade.php
@@ -7,56 +7,58 @@ Contact
 @endsection
 @section('main')
     <main>
-      <!-- Banner -->
-      <div id="banner">
+     <!-- Banner -->
+     <div id="banner">
         <div class="banner-container">
-          <div class="sub-section">
-            <!-- 標題區塊&SCROLL -->
-            <div class="tittle-en">
-              <h3>CONTACT</h3>
+            <div class="sub-section">
+                <!-- 標題區塊&SCROLL -->
+                <div class="tittle-en">
+                    <h3>CONTACT</h3>
+                </div>
+                <div class="tittle-ch">
+                    <h4>聯絡我們</h4>
+                </div>
+                <!-- SCROLL示意 -->
+                <div class="scroll-box">
+                    <div class="scroll-img">
+                        <img src="{{asset('image/icon/Equipment/gray_dwon.svg')}}" alt="" />
+                    </div>
+                    <div class="scroll-text">
+                        <span>SCROLL</span>
+                    </div>
+                </div>
+                <!-- 灰色色塊 -->
+                <div class="sup-bg">
+                    <div class="color-block"></div>
+                </div>
             </div>
-            <div class="tittle-ch">
-              <h4>聯絡我們</h4>
+            <!-- 公司主圖 -->
+            <div class="main-section">
+                <div class="banner-img">
+                    <div class="img">
+                        <img src="{{asset('image/contact/Rectangle 22 (1).png')}}" alt="" />
+                        <div class="main-img-color"></div>
+                    </div>
+
+                    <!-- banner直版進度條 -->
+                    <div class="progress-bar-l">
+                        <div class="progress-step1">INPUT</div>
+                        <div class="progress-step2">CONFIRM</div>
+                        <div class="progress-step3">COMPLATE</div>
+                    </div>
+                    <!-- SCROLL示意 -->
+                    <div class="scroll-box">
+                        <div class="scroll-img">
+                            <img src="{{asset('image/icon/Equipment/gray_dwon.svg')}}" alt="" />
+                        </div>
+                        <div class="scroll-text">
+                            <span>SCROLL</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <!-- SCROLL示意 -->
-            <div class="scroll-box">
-              <div class="scroll-img">
-                <img src="./img/functionIcon/gray_dwon.svg" alt="" />
-              </div>
-              <div class="scroll-text">
-                <span>SCROLL</span>
-              </div>
-            </div>
-            <!-- 灰色色塊 -->
-            <div class="sup-bg">
-              <div class="color-block"></div>
-            </div>
-          </div>
-          <!-- 公司主圖 -->
-          <div class="main-section">
-            <div class="banner-img">
-              <div class="img">
-                <img src="./img/範例.jpg" alt="" />
-                <div class="main-img-color"></div>
-              </div>
-              <!-- 頁面導覽塊 -->
-              <div class="progress-bar">
-                <ul>
-                  <li>經營理念</li>
-                  <li>公司沿革</li>
-                  <li>公司簡介</li>
-                </ul>
-              </div>
-                            <!-- banner直版進度條 -->
-                            <div class="progress-bar-l">
-                                <div class="progress-step1">INPUT</div>
-                                <div class="progress-step2">CONFIRM</div>
-                                <div class="progress-step3">COMPLATE</div>
-                              </div>
-            </div>
-          </div>
         </div>
-      </div>
+    </div>
       <!-- 聯絡表單區塊 -->
       <div id="contact-id">
         <!-- 直版表單進度條 -->
